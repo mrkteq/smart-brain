f64b6ad58c050364314ba0b3aa84619807f15ee8
mrkteq <markanthonytucker@gmail.com>
2023-07-24 17:25:37 +0200

    error handling

--- src/App.js
+++ src/App.js
@@ -126,6 +126,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, {entries: count}))
           })
+          .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(result))
       }
